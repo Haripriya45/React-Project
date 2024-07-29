9f58fe6b1102f364cb24ca33db5636a938569bb2
Haripriya Palleti <haripriyapalleti5@gmail.com>
2024-07-29 16:25:27 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
+import '../node_modules/bootstrap/dist/js/bootstrap.js';
+import SignupAPI from './API Reactjs Project/SignupAPI.js';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+ 
+   // <App />
+<SignupAPI/>
+);
